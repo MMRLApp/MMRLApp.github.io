bfd5cf31f2245551d74b3e407bce88c2300f4a69
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-02-01 16:15:08 +0100

    fixes

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -28,80 +28,9 @@ export default defineConfig({
       },
     ],
   ],
-  transformHead: ({ pageData }) => {
-    const head: HeadConfig[] = [];
-
-    if (pageData.frontmatter.description) {
-      head.push([
-        "meta",
-        {
-          property: "og:description",
-          content: pageData.frontmatter.description,
-        },
-      ]);
-    }
-
-    if (pageData.frontmatter.image) {
-      head.push([
-        "meta",
-        { property: "og:image", content: pageData.frontmatter.image },
-      ]);
-    }
-
-    head.push([
-      "meta",
-      {
-        property: "og:url",
-        content: `https://mmrl.dev${pageData.relativePath}`,
-      },
-    ]);
-
-    head.push(["meta", { property: "og:type", content: "website" }]);
-
-    head.push(["meta", { property: "og:site_name", content: "MMRL" }]);
-
-    head.push(["meta", { property: "og:locale", content: "en_US" }]);
-
-    head.push(["meta", { name: "twitter:card", content: "summary" }]);
-
-    head.push(["meta", { name: "twitter:site", content: "@Der_Googler" }]);
-
-    head.push(["meta", { name: "twitter:creator", content: "@Der_Googler" }]);
-
-    head.push([
-      "meta",
-      { name: "twitter:title", content: pageData.frontmatter.title },
-    ]);
-
-    head.push([
-      "meta",
-      {
-        name: "twitter:description",
-        content: pageData.frontmatter.description,
-      },
-    ]);
-
-    if (pageData.frontmatter.image) {
-      head.push([
-        "meta",
-        { name: "twitter:image", content: pageData.frontmatter.image },
-      ]);
-
-      head.push([
-        "meta",
-        { name: "twitter:image:alt", content: pageData.frontmatter.title },
-      ]);
-    }
-
-    return head;
-  },
   buildEnd: async (config: SiteConfig) => {
     const publicApi = resolve(config.outDir, "api");
-    // const repositoriesYaml = resolve(publicApi, "repositories.yaml");
-    const publicRepoList = resolve(publicApi, "gg");
-
-    // const readContent = await readFile(repositoriesYaml, "utf8");
-    // const par = parse(readContent);
+    const publicRepoList = resolve(publicApi, "repositories.json");
 
     console.log("Writing repositories.json");
     await writeFile(publicRepoList, repositoriesJSONstringify);
