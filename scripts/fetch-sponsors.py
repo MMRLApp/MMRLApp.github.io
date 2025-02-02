c439a9bc8eb47959fe3613f88a20e2fc5535371f
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-02-02 13:15:27 +0100

    .

--- scripts/fetch-sponsors.py
+++ scripts/fetch-sponsors.py
@@ -27,6 +27,27 @@ query {
       }
     }
   }
+  user(login: "MMRLApp") {
+     sponsorshipsAsMaintainer(activeOnly: false, first: 100) {
+      nodes {
+        sponsorEntity {
+          ... on User {
+            login
+            avatarUrl
+            url
+          }
+          ... on Organization {
+            login
+            avatarUrl
+            url
+          }
+        }
+        tier {
+          monthlyPriceInCents
+        }
+      }
+    }
+  }
 }
 """
 
